fe24f7c087077488c40ae9c56c3b025c79f9af9a
Muhammad Rayhan Denel <muhammad.rayhan13@ui.ac.id>
2022-09-11 19:54:21 +0700

    selesai raw

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
+def show_catalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog': data_barang_katalog,
+        'nama': 'Muhammad Rayhan Denel',
+        'npm' : '2106752161'
+    }
+    return render(request, "katalog.html", context)
 # TODO: Create your views here.
